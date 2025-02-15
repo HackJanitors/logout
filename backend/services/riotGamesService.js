2c08e2b4fa363f6c96691cceb965016d650697d3
Jo An <positivelyjon@gmail.com>
2025-02-15 23:17:48 +0800

    fix: make riot api get hours

--- backend/services/riotGamesService.js
+++ backend/services/riotGamesService.js
@@ -1,5 +1,5 @@
 const { formatUrlWithQueryParams } = require("../utils")
-const fetch = require("node-fetch")
+const axios = require('axios')
 
 require("dotenv").config()
 
@@ -8,26 +8,26 @@ const RIOT_GAMES_URL = process.env.RIOT_GAMES_URL
 
 const getRiotTodayHours = async (childRiotId) => {
     const [gameName, tagLine] = childRiotId.split("#")
-    const uri = formatUrlWithQueryParams(
+    const puuid = await axios.get(formatUrlWithQueryParams(
         RIOT_GAMES_URL + `/riot/account/v1/accounts/by-riot-id/${gameName}/${tagLine}`, 
         {
             "api_key": RIOT_API_KEY
         }
-    )
-    console.log(`Fetching from ${uri}`)
-    const puuid = await fetch(uri).then(response => response.json().puuid)
-  
+    )).then(response => response.data.puuid)
+
     const startTime = new Date();
     startTime.setHours(0, 0, 0, 0); // Start of today
+    
     const endTime = new Date();
     endTime.setHours(23, 59, 59, 999); // End of today
+    startTime.setDate(endTime.getDate() - 3)
+
 
     const startTimeUnix = Math.floor(startTime.getTime() / 1000);
     const endTimeUnix = Math.floor(endTime.getTime() / 1000);
 
-
     try {
-        const matchIdsResponse = await fetch(
+        const matchIds = await axios.get(
             formatUrlWithQueryParams(
                 `${RIOT_GAMES_URL}/lol/match/v5/matches/by-puuid/${puuid}/ids`, 
                 {
@@ -36,27 +36,22 @@ const getRiotTodayHours = async (childRiotId) => {
                     api_key: RIOT_API_KEY
                 }
             )
-        );
+        ).then(response => response.data);
+        console.log(`Fetched match ids ${matchIds}`)
 
-        if (!matchIdsResponse.ok) {
-            const errorMessage = await matchIdsResponse.text();
-            throw new Error(`Failed to fetch match IDs from Riot Games API: ${errorMessage}`);
-        }
-
-        const matchIds = await matchIdsResponse.json();
         let totalGameDuration = 0;
 
         for (const matchId of matchIds) {
-            const matchResponse = await fetch(
-                `${RIOT_GAMES_URL}/lol/match/v5/matches/${matchId}?api_key=${RIOT_API_KEY}`
-            );
+            const gameDuration = await axios.get(
+                formatUrlWithQueryParams(
+                    `${RIOT_GAMES_URL}/lol/match/v5/matches/${matchId}?api_key=${RIOT_API_KEY}`, 
+                    {
+                        api_key: RIOT_API_KEY
+                    }
+                )
+            ).then(response => response.data.info.gameDuration);
 
-            if (!matchResponse.ok) {
-                throw new Error(`Failed to fetch match data for match ID: ${matchId}`);
-            }
-
-            const matchJson = await matchResponse.json();
-            totalGameDuration += matchJson.info.gameDuration;
+            totalGameDuration += gameDuration
         }
 
         // Convert total game duration from seconds to hours and format to 2 decimal places
