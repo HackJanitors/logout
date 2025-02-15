dbc7453460ecc0906d23869101aabf296a446ab8
Jaehyeok Choi <77jack0105@gmail.com>
2025-02-15 16:27:02 +0800

    feat: create routes

---
+++ backend/controllers/user.js
@@ -0,0 +1,39 @@
+import Child from '../models/child'
+import Guardian from '../models/guardian'
+
+const express = require('express')
+const app = express()
+
+
+app.post('/admin/:guardianId', async (req, res) => {
+    const { guardianId, childId, guardianUsername, guardianWalletId, childWalletId, childUsername, riotId, goal, dailyRate, weeklyRate, monthlyRate } = req.body;
+
+    const newGuardian = new Guardian({
+        id: guardianId,
+        username: guardianUsername,
+        walletId: guardianWalletId,
+    });
+
+    const savedGuardian = await newGuardian.save();
+
+    const newChild = new Child({
+        id: childId,
+        username: childUsername,
+        guardianId: savedGuardian._id,
+        riotId: riotId,
+        walletId: childWalletId,
+        goal: goal,
+        dailyRate: dailyRate,
+        weeklyRate: weeklyRate,
+        monthlyRate: monthlyRate,
+    });
+
+    const savedChild = await newChild.save();
+
+    res.status(201).json({
+        message: "Guardian and Child created successfully",
+        guardian: savedGuardian,
+        child: savedChild
+    });
+});
+
