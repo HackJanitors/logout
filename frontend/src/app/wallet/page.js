cfb39b9cefcc15243178ce78d471d66a5b1ee1b9
NicT5 <nicctang@gmail.com>
2025-02-15 20:31:40 +0800

    Update wallet page

--- frontend/src/app/wallet/page.js
+++ frontend/src/app/wallet/page.js
@@ -1,7 +1,22 @@
+"use client"
+
+import { Button } from "@/components/ui/button"
+import { useState } from "react"
+
 export default function Wallet() {
+    const [walletAmount, setwalletAmount] = useState(5)
+
     return (
-        <div>
-            Test
+        <div className="mt-10 ml-10 w-full">
+            <div id="balance" className="border w-3/4 p-5 rounded-lg shadow-md flex flex-col">
+                <div>
+                    <h1 className="text-3xl font-semibold "> Balance </h1>
+                </div>
+                <div className="mt-8 text-5xl font-extrabold flex flex-row">
+                    $ {walletAmount.toFixed(2)}
+                    <Button variant="outline" className="ml-auto bg-green-500"> Top-Up </Button>
+                </div>
+            </div>
         </div>
     )
 }
