9961104d03591f712cdfd757e8ef19bd4820bdd2
Jo An <positivelyjon@gmail.com>
2025-02-15 17:17:10 +0800

    feat: add mongodb connection to server

--- backend/server.js
+++ backend/server.js
@@ -3,17 +3,45 @@ const applyMiddleware = require('./middlewares')
 const adminRoutes = require('./controllers/admin')
 const userRouters = require('./controllers/user')
 const dashboardRoutes = require('./controllers/dashboard')
+const mongoose = require("mongoose")
+require('dotenv').config()
 
 const app = express()
-const port = 8000
+const port = '8000'
 
-applyMiddleware(app)
+const mongodbUsername = process.env.MONGODB_USERNAME
+const mongodbPassword = process.env.MONGODB_PASSWORD
 
-app.use('/admin', adminRoutes);
-app.use('/dashboard', dashboardRoutes)
-app.use('/user', userRouters)
+const uri = `mongodb+srv://${mongodbUsername}:${mongodbPassword}@logoff.dsoac.mongodb.net/?retryWrites=true&w=majority&appName=LogOff`;
+const clientOptions = { serverApi: { version: '1', strict: true, deprecationErrors: true } };
 
-app.listen(port, () => {
-    console.log(`Server is listening on http://localhost:${port}`)
-})
+async function connectToMongoDB() {
+    try {
+        // Create a Mongoose client with a MongoClientOptions object to set the Stable API version
+        await mongoose.connect(uri, clientOptions);
+        await mongoose.connection.db.admin().command({ ping: 1 });
+        console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    } catch (error) {
+        console.log("Failed to connect to MongoDB", error)
+    }
+}
+async function startServer() {
+  // Connect to the database
+  await connectToMongoDB();
 
+  applyMiddleware(app);
+
+  app.use('/admin', adminRoutes);
+  app.use('/dashboard', dashboardRoutes);
+  app.use('/user', userRouters);
+
+  app.get('/', (req, res) => {
+    res.send('Welcome to the API');
+  });
+
+  app.listen(port, () => {
+    console.log(`Server is listening on http://localhost:${port}`);
+  });
+}
+
+startServer()
