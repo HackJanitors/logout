44f2854216bfce301bcfcd302a534945eaa0ed3a
Jaehyeok Choi <77jack0105@gmail.com>
2025-02-15 15:17:25 +0800

    child_schema

---
+++ backend/models/child.js
@@ -0,0 +1,17 @@
+import mongoose from 'mongoose';
+const { Schema, model } = mongoose;
+
+const childSchema = new Schema({
+    Id: String,
+    username: String,
+    guardianId: String,
+    riotId: String,
+    walletId: String,
+    goal: Number,
+    dailyRate: Number,
+    weeklyRate: Number,
+    monthlyRate: Number
+});
+
+const Child = model('Child', childSchema);
+export default Child;
